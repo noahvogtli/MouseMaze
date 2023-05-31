6cfa66f048c4bdd9320a7f4c271698b59e89f391
Noah Vogtli <noahvogtli88@gmail.com>
2023-05-31 14:24:04 -0400

    change: using gridPane instead of pane

--- mousemaze/src/main/java/maze/view/GUITest.java
+++ mousemaze/src/main/java/maze/view/GUITest.java
@@ -1,35 +1,89 @@
 package maze.view;
 
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
-public class GUITest  extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GUITest extends Application 
+{
+    private static final int GRID_SIZE = 50; // GridPane size
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Translate Example");
+        GridPane gridPane = new GridPane();
+        File file = new File("mousemaze/src/main/java/maze/data/art/Mouse.png");
+        Image image = new Image(file.toURI().toString());
+        ImageView imageView = new ImageView(image);
+        gridPane.add(imageView, 0, 0);
 
-        Button button = new Button("Click me!");
-        button.setLayoutX(50); // Set initial x-position
-        button.setLayoutY(50); // Set initial y-position
+        // Set up row and column constraints
+        for (int i = 0; i < GRID_SIZE; i++) {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setPercentWidth(100.0 / GRID_SIZE);
+            gridPane.getColumnConstraints().add(column);
 
-        button.setOnAction(event -> {
-            double newX = button.getLayoutX() + 10; // Move 10 units to the right
-            double newY = button.getLayoutY() + 10; // Move 10 units down
-            button.setLayoutX(newX);
-            button.setLayoutY(newY);
+            RowConstraints row = new RowConstraints();
+            row.setPercentHeight(100.0 / GRID_SIZE);
+            gridPane.getRowConstraints().add(row);
+        }
+
+        Scene scene = new Scene(gridPane, 1500, 800);
+        scene.setOnKeyPressed(event -> {
+            KeyCode keyCode = event.getCode();
+            int column = GridPane.getColumnIndex(imageView);
+            int row = GridPane.getRowIndex(imageView);
+
+            switch (keyCode) {
+                case UP:
+                    if (row > 0) {
+                        GridPane.setRowIndex(imageView, row - 1);
+                    }
+                    break;
+                case DOWN:
+                    if (row < GRID_SIZE - 1) {
+                        GridPane.setRowIndex(imageView, row + 1);
+                    }
+                    break;
+                case LEFT:
+                    if (column > 0) {
+                        GridPane.setColumnIndex(imageView, column - 1);
+                    }
+                    break;
+                case RIGHT:
+                    if (column < GRID_SIZE - 1) {
+                        GridPane.setColumnIndex(imageView, column + 1);
+                    }
+                    break;
+                default:
+                    break;
+            }
         });
 
-        Pane pane = new Pane(button);
-        Scene scene = new Scene(pane, 300, 200);
+        // Set focus on the scene to capture key events
+        scene.getRoot().requestFocus();
+
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Image Move App");
         primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
 
--- mousemaze/src/main/java/maze/view/MouseMazeGUI.java
+++ mousemaze/src/main/java/maze/view/MouseMazeGUI.java
@@ -12,39 +12,73 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MouseMazeGUI extends Application
 {
-    private Pane pane;
+    private static final int GRID_SIZE = 50; // GridPane size
 
     @Override
-    public void start(Stage primaryStage) throws Exception 
-    {
-        ImageView mouseView = createImage("mousemaze/src/main/java/maze/data/art/Mouse.png");
-        ImageView pathView = createImage("mousemaze/src/main/java/maze/data/art/Path.png");
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(pathView,mouseView);
-        pane = new Pane();
-        pane.setPrefSize(1500,900);
-        pane.setMaxSize(1500, 900);
-        pane.getChildren().addAll(stackPane);
-        Scene scene = new Scene(pane);
-        scene.setOnKeyPressed(e -> {
-                if ( e.getCode() == KeyCode.RIGHT) {
-                    stackPane.setLayoutX(stackPane.getLayoutX() + 50);
-                } else if (e.getCode() == KeyCode.LEFT) {
-                    stackPane.setLayoutX(stackPane.getLayoutX() - 50);
-                } else if (e.getCode() == KeyCode.UP) {
-                    stackPane.setLayoutY(stackPane.getLayoutY() - 50);
-                } else if (e.getCode() == KeyCode.DOWN) {
-                    stackPane.setLayoutY(stackPane.getLayoutY() + 50);
-                }
+    public void start(Stage primaryStage) {
+        GridPane gridPane = new GridPane();
+        File file = new File("mousemaze/src/main/java/maze/data/art/Mouse.png");
+        Image image = new Image(file.toURI().toString());
+        ImageView imageView = new ImageView(image);
+        gridPane.add(imageView, 0, 0);
+
+        // Set up row and column constraints
+        for (int i = 0; i < GRID_SIZE; i++) {
+            ColumnConstraints column = new ColumnConstraints();
+            column.setPercentWidth(100.0 / GRID_SIZE);
+            gridPane.getColumnConstraints().add(column);
+
+            RowConstraints row = new RowConstraints();
+            row.setPercentHeight(100.0 / GRID_SIZE);
+            gridPane.getRowConstraints().add(row);
+        }
+
+        Scene scene = new Scene(gridPane, 1500, 800);
+        scene.setOnKeyPressed(event -> {
+            KeyCode keyCode = event.getCode();
+            int column = GridPane.getColumnIndex(imageView);
+            int row = GridPane.getRowIndex(imageView);
+
+            switch (keyCode) {
+                case UP:
+                    if (row > 0) {
+                        GridPane.setRowIndex(imageView, row - 1);
+                    }
+                    break;
+                case DOWN:
+                    if (row < GRID_SIZE - 1) {
+                        GridPane.setRowIndex(imageView, row + 1);
+                    }
+                    break;
+                case LEFT:
+                    if (column > 0) {
+                        GridPane.setColumnIndex(imageView, column - 1);
+                    }
+                    break;
+                case RIGHT:
+                    if (column < GRID_SIZE - 1) {
+                        GridPane.setColumnIndex(imageView, column + 1);
+                    }
+                    break;
+                default:
+                    break;
+            }
         });
+
+        // Set focus on the scene to capture key events
+        scene.getRoot().requestFocus();
+
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Image Move App");
         primaryStage.show();
     }
 
